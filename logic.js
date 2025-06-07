1bc060cc472aa40a5976d4fb472ab016061cda8c
Anuprash Subedi <info.anuprash@gmail.com>
2025-06-07 08:33:49 +0000

    learnt how to get time and date and ensured pproper formatting of time by using padStart(2,'0') for sinlge digit numbers

--- logic.js
+++ logic.js
@@ -1,25 +1,32 @@
 
 
-let hour=document.getElementById("hourBox");
+let hour=document.getElementById("hourBox"); // gets element by ID
 let minute=document.getElementById("minuteBox");
 let second=document.getElementById("secondBox");
 
+let date=new Date(); // The new Date() has proper date and time for the region
 
-setInterval(()=>{
+
+setInterval(()=>{ 
 
     let date=new Date();
 
-    let hours=date.getHours();
+    let hours=date.getHours(); //Getting specific time element from new Date() function
     let minutes=date.getMinutes();
     let seconds=date.getSeconds();
 
-    hour.innerHTML=hours;
-    minute.innerHTML=minutes;
-    second.innerHTML=seconds;
+    // Manipulation using DOM
 
     
+    hour.innerHTML=String(hours).padStart(2,'0');  /*  String(hours) :  converts hours to string
+
+                                                       padStart(2,'0'): adds padding of 0 unless
+                                                                        the no. of characters become
+                                                                        two                */ 
+    minute.innerHTML=String(minutes).padStart(2,'0');
+
+    second.innerHTML=String(seconds).padStart(2,'0');
 
 }
-,1000);
-
+,1000); // 1000 milliseconds =1 second : so the date gets fetched every second 
 
